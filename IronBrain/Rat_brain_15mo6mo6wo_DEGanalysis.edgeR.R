904b3348e163595389062ad7e4baa8d9e584df7d
kujin <kkwon@unist.ac.kr>
2020-03-04 22:39:33 +0900

    Filtering logic fix

--- IronBrain/Rat_brain_15mo6mo6wo_DEGanalysis.edgeR.R
+++ IronBrain/Rat_brain_15mo6mo6wo_DEGanalysis.edgeR.R
@@ -1,9 +1,9 @@
 library(edgeR)
 tbl = read.table('Rat_Brain_15M_6M_6W_count.csv', header = TRUE, sep = '\t', row.names = 1)
 tbl_rpkm = read.table('Rat_Brain_15M_6M_6W_rpkm.csv', header = TRUE, sep = '\t', row.names = 1)
-select = apply(tbl_rpkm, 1, function(x) sum(x >= 1) == 36 )
 Outlier = tbl[,23, drop = FALSE]
 tbl = tbl[,-c(23)]
+select = apply(tbl_rpkm, 1, function(x) sum(x >= 1) == 36 )
 Sample = colnames(tbl)
 Group = c('E','A','D','D','D','A','D','A','A','E','E','E','C','C','A','A','A',rep('C', times = 8), rep('B', times = 10))
 Age = c(rep(c('15M','6M','6W'),c(8,17,10)))
