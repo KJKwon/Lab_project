23a40cbcf5641b4da6758a4966da9adba8a14fb8
kujin <kkwon@unist.ac.kr>
2020-08-05 13:10:11 +0900

    Bug fix

--- CRISPRi/bootstrapping_test.py
+++ CRISPRi/bootstrapping_test.py
@@ -4,7 +4,6 @@ from scipy.stats import spearmanr
 
 def sgRNA2ref(raw_sgRNA_list, sgRNA_DB,sampling_num):
     sgRNA_num2count = dict()
-    random.seed(123)
     tmp_sgRNA_sampling = random.choices(raw_sgRNA_list, k = sampling_num)
     for tmp_sgRNA in tmp_sgRNA_sampling:
         if tmp_sgRNA in sgRNA_DB:
@@ -33,7 +32,7 @@ def fasta2list(file_name):
 
 def cor_output(sgRNA1, sgRNA2):
     total_sgRNA_clean = set(sgRNA1.keys()) & set(sgRNA2.keys())
-    total_sgRNA_clean = list(total_name)
+    total_sgRNA_clean = list(total_sgRNA_clean)
     query1_clean = []
     query2_clean = []
     for tmp_sgRNA_num in total_sgRNA_clean:
@@ -41,7 +40,7 @@ def cor_output(sgRNA1, sgRNA2):
         query2_clean_sgRNA = sgRNA2[tmp_sgRNA_num]
         query1_clean.append(query1_clean_sgRNA)
         query2_clean.append(query2_clean_sgRNA)
-    corr, _ = spearmanr(Query_1_clean, Query_2_clean) 
+    corr, _ = spearmanr(query1_clean, query2_clean) 
     
     return corr
     
@@ -50,12 +49,12 @@ def cor_output(sgRNA1, sgRNA2):
 if len(sys.argv) < 2:
     print ('[USAGE] bootstrapping_test.py [FASTA 1] [FASTA 2]')
     sys.exit(1)
-    
+else:    
     f_fasta1_nm = sys.argv[1]
     f_fasta2_nm = sys.argv[2]
-    f_out_nm = f_fasta1_nm.split('_S')[0] + '_' + f_fasta2_nm.split('_S') +'_correlation_bootstrap_output.txt'
-    f_out = (f_out_nm,'w')
-    f_ref = open('broadgpp-dolcetto-targets-set.txt','r')
+    f_out_nm = f_fasta1_nm.split('_S')[0] + '_' + f_fasta2_nm.split('_S')[0] +'_correlation_bootstrap_output.txt'
+    f_out = open(f_out_nm,'w')
+    f_ref = open('broadgpp-dolcetto-targets-set_mageck.txt','r')
     sgRNA2num = dict()
     for line in f_ref:
         token = line.strip().split()
@@ -67,10 +66,10 @@ if len(sys.argv) < 2:
     fasta2_sgRNA = fasta2list(f_fasta2_nm)
     f_out.write('Sampling_num\tCorr_coeff\n')
     for i in [0.2,0.25,0.5,1,2,3,4,5]:
-        select_num = int(length(fasta1_sgRNA)*i)
+        select_num = int(len(fasta1_sgRNA)*i)
         fasta1_ref_sgRNA = sgRNA2ref(fasta1_sgRNA,sgRNA2num,select_num)
         fasta2_ref_sgRNA = sgRNA2ref(fasta2_sgRNA,sgRNA2num,select_num)
-        tmp_cor = cor_output(fasta1_ref_sgRNA, fasta2_ref,sgRNA)
+        tmp_cor = cor_output(fasta1_ref_sgRNA, fasta2_ref_sgRNA)
         f_out.write('%f\t%.3f\n'%(i,tmp_cor))
 
 f_out.close()
