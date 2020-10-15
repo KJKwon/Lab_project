8e2f061ca73272e8ee3e27f3f82be133501f9730
kujin <kkwon@unist.ac.kr>
2020-10-15 18:53:06 +0900

    Add PDF save options
    PDF saving options for publish

--- IronBrain/FerricChloride+Cytotoxic_WST-1_bargraph.R
+++ IronBrain/FerricChloride+Cytotoxic_WST-1_bargraph.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(reshape2)
+library(grid)
 tbl = read.table('SH-SY5Y_IronToxicityTest_FeCl2_24h_WST-1_results.csv', row.names = 1, header = TRUE)
 conc.list = c('Control','100uM','200uM','500uM','1mM','2mM','5mM','10mM')
 colnames(tbl) = conc.list
@@ -20,14 +21,17 @@ tbl.plot = tbl.plot[,3:5]
 colnames(tbl.plot) = c('variable', 'value', 'sd')
 colnames(tbl.dotplot)[2] = c('variable')
 tbl.plot$value = factor(tbl.plot$value, levels = tbl.plot$value)
+pdf(file = "Figure4_1_SH-SY5Y_IronTreat_WST-1_Cytotoxicity_barplot_v1.pdf", width = 3.1, height = 3)
 p = ggplot(tbl.plot,aes(x = value, y = variable)) + geom_bar(stat = 'identity', position = 'dodge', width = 0.5, colour = 'black',
-                                                             fill = 'white')+
-  geom_errorbar(aes(x = value, ymin = variable - sd, ymax = variable + sd), position = position_dodge(0.9), width = 0.15)+
-  geom_point(aes(x = variable, y = value),tbl.dotplot, position = position_dodge(0.9))+
-  theme(text = element_text(size = 20), legend.title = element_blank(), panel.background = element_blank(),
+                                                             fill = 'white', lwd = 0.8)+
+  geom_errorbar(aes(x = value, ymin = variable - sd, ymax = variable + sd), position = position_dodge(0.9), width = 0.3)+
+  geom_point(aes(x = variable, y = value),tbl.dotplot, position = position_dodge(0.9), size = 0.4)+
+  theme(text = element_text(size = 9), legend.title = element_blank(), panel.background = element_blank(),
         panel.border = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(size = 1, colour = 'black'),
-        axis.text.x = element_text(colour = 'black', size = 15), axis.text.y = element_text(colour = 'black', size = 15),
-        axis.title.x = element_blank()) + 
-  ylab('% WST-1 Absorbance\n(compared to control)') + scale_y_continuous(expand = expand_scale(mult = c(0, .1)))
+        axis.text.x = element_text(colour = 'black', size = 9), axis.text.y = element_text(colour = 'black', size = 9),
+        axis.title.x = element_text(colour = 'black', size = 9), axis.ticks = element_line(size = 0.8)) + 
+  ylab('% WST-1 Absorbance\n(compared to control)') + xlab('FeCl2 Concentration (mM)')+scale_y_continuous(expand = expand_scale(mult = c(0, .1)))
 
 plot(p)
+
+dev.off()
