59b95c2a24bbdb5958c2ff19a7e5b5fb54b32faf
kujin <kkwon@unist.ac.kr>
2021-03-08 13:57:38 +0900

    Edit color settings
    barplot border color

--- IronBrain/siRNA_plot_Cq.R
+++ IronBrain/siRNA_plot_Cq.R
@@ -17,17 +17,18 @@ for(i in 0:(length(tbl$Cq)/3-1)){
 tbl.mean = data.frame(Avg.mean,Avg.sd,Avg.group,Avg.Target)
 
 p = ggplot(tbl.mean,aes(x = Avg.group, y = Avg.mean, fill = Avg.Target))+
-  geom_bar(position = position_dodge(),stat = 'identity')+
+  geom_bar(position = position_dodge(),stat = 'identity', colour = 'black')+
   geom_point(aes(x = Group, y = Cq, color = Target),
              data = tbl,stat='identity', position = position_dodge(0.9), inherit.aes = FALSE,
              size = 1)+
   geom_errorbar(aes(ymin = Avg.mean - Avg.sd, ymax = Avg.mean + Avg.sd), position =position_dodge(0.9), width = 0.15)+
   scale_color_manual(values = c('black','black'))+
-    theme(legend.title = element_blank(), plot.title = element_text(size = 30, hjust = 0.5,margin = margin(0,0,40,0)), 
+  scale_fill_manual(values = c('white','grey50'))+
+  theme(legend.title = element_blank(), plot.title = element_text(size = 30, hjust = 0.5,margin = margin(0,0,40,0)), 
         panel.background = element_blank(), axis.line = element_line(size = 1, colour = 'black'), 
         legend.position = 'none', axis.text.x = element_text(size = 15, colour = 'black', angle = 45, hjust = 1),
         axis.text.y = element_text(size = 15, colour = 'black'), axis.title.y = element_text(size = 20))+ 
   xlab(NULL) +ylab('Cq')+ 
   scale_y_continuous(expand = c(0,0), limits = c(0, 25))+
-  labs(title = "Gadd45b siRNA 48h (n = 2)")+ scale_fill_grey()
+  labs(title = "Gadd45b siRNA 48h (n = 2)")
 plot(p)
